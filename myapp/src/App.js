22d3382671df394ce877fc79473f997e96e1a428
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-08-23 19:50:05 +0100

    Add initial routes

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,15 @@
-import './App.css';
+import styles from './App.module.css';
+import { Route, Routes } from 'react-router-dom/';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Badminton</p>
-      </header>
+    <div className={styles.App}>
+      <Routes>
+        <Route exact path="/" element={<p>home</p>} />
+        <Route exact path="/league/:id" element={<p>league</p>} />
+        <Route exact path="/session/:id" element={<p>session</p>} />
+        <Route path="*" element={<p>Not found</p>} />
+      </Routes>
     </div>
   );
 }
