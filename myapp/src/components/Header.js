2f8225b7dac65b93de415db3d4a7e6eaf37cf6c3
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-08-24 09:37:35 +0100

    Add breadcrumb to navigation

--- myapp/src/components/Header.js
+++ myapp/src/components/Header.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocation } from 'react-router-dom';
+import { BreadCrumb } from 'primereact/breadcrumb';
 
 const Header = () => {
+  const [items, setItems] = useState([]);
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = async () => {
+      if (location.pathname.startsWith('/league/'))
+        setItems()
+      else if (location.pathname.startsWith('/session/'))
+        setItems([{ label: 'League' }])
+      else {
+        console.log(location)
+      }
+    };
+    path();
+  }, [location]);
+  // const items = [{ label: 'Electronics' }, { label: 'Computer' }];
+  const home = { icon: 'pi pi-home', url: '/BadmintonLeague/' }
   return (
     <header>
-      
+      <BreadCrumb model={items} home={home} />
     </header>
   )
 }
