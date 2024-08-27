bb67e5214ab380ce65164ebbf67a5e3b7e68b5e5
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-08-27 19:07:22 +0100

    Add league template and create session dialog

--- myapp/src/pages/league/League.js
+++ myapp/src/pages/league/League.js
@@ -1,9 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Button } from 'primereact/button';
+import CreateSession from '../session/CreateSession'
 
 const League = () => {
+  const [visible, setVisible] = useState(false);
   return (
     <div>
-      <p>League Detail</p>
+      <p>League Leaderboard</p>
+      <Button label="Session" icon="pi pi-plus" onClick={()=>{setVisible(true)}}/>
+      <CreateSession visible={visible} setVisible={setVisible}/>
     </div>
   )
 }
