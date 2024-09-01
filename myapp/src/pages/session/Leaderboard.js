016b50006da4c79813ccc45ef9eec8686c547a05
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-09-01 22:04:49 +0100

    Add league and session name in leaderboard

--- myapp/src/pages/session/Leaderboard.js
+++ myapp/src/pages/session/Leaderboard.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+import { useSessionContext } from "../../context/SessionContext";
 
 const Leaderboard = () => {
   const data = [{player: 'Hussain', wins: 2}, {player: 'Tauseef', wins: 1}]
+  const { sessionContext, setSessionContext } = useSessionContext();
 
   return (
     <div className="col-12 lg:col-6">
       <div className="surface-0 shadow-2 p-3 border-1 border-50 border-round">
+      <span className="block text-500 font-medium mb-3">{sessionContext}</span>
         <DataTable value={data} stripedRows showGridlines size="small">
           <Column field="player" header="Player"></Column>
           <Column field="wins" header="Wins"></Column>
