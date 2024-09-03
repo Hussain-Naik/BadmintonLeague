5a349d1d18df0c9cc983bf7cf73ff710e11ccd98
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-09-03 17:57:09 +0100

    Add google sheets api url

--- myapp/src/api/axiosDefaults.js
+++ myapp/src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://pp5-synergy.onrender.com/";
+axios.defaults.baseURL = "https://script.google.com/macros/s/AKfycbyUEhT9WcnKAgikb0kFQ5S1plMGP5RHZt_eh5WmpfmwzsUmGc6Q8K9jCBFOftcLWiddsg/exec";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
