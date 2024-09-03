1979544bc3042a7e2673550b42dbfabf8cb83f99
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-09-03 19:30:43 +0100

    Add api to google sheet

--- myapp/src/api/axiosDefaults.js
+++ myapp/src/api/axiosDefaults.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://script.google.com/macros/s/AKfycbyUEhT9WcnKAgikb0kFQ5S1plMGP5RHZt_eh5WmpfmwzsUmGc6Q8K9jCBFOftcLWiddsg/exec";
+axios.defaults.baseURL = "https://script.google.com/macros/s/AKfycbz8loqyuTAf4sDGuFvmFqUTCQx1lG-rXle71GsY-CJylJZUzVMJj8N09LdTrPviuZKgxw/exec";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
-axios.defaults.withCredentials = true;
 
 export const axiosReq = axios.create();
--- myapp/src/pages/home/Home.js
+++ myapp/src/pages/home/Home.js
@@ -1,9 +1,17 @@
 import React, { useEffect, useState } from "react";
 import LeagueItems from "../league/LeagueItems";
+import { axiosReq } from "../../api/axiosDefaults";
 
 const Home = () => {
 
-  const handleMount = () => {};
+  const handleMount = async () => {
+    try {
+      const { data } = await axiosReq.get();
+      console.log(data.data)
+    } catch (err) {
+      console.log(err)
+    }
+  };
 
   useEffect(() => {
     handleMount();
