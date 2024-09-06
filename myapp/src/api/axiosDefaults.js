8de35c7ee23eb5c7a3d5ff4a75bb16114dc7d088
Hussain Naik <131135414+Hussain-Naik@users.noreply.github.com>
2024-09-06 10:19:40 +0100

    Add latest deployment url

--- myapp/src/api/axiosDefaults.js
+++ myapp/src/api/axiosDefaults.js
@@ -5,7 +5,7 @@ const axiosAPI = axios.create({
 });
 
 const axiosReq = axios.create({
-    baseURL: "https://script.google.com/macros/s/AKfycbxbeNEAokBC6j2mtLnMPYVqEpWWB1DNo8GVQ-oKpUky-5exfBX_cDaLHWQsEhf5AtIzIQ/exec"
+    baseURL: "https://script.google.com/macros/s/AKfycbxpQ3Yb7-0UE5bEXyn-PTTF0cmfKCPJ4sGI5MHdfgYpZeDD9a3bQ-t1jJcCN2G_qWKUWw/exec"
 });
 
 export {
